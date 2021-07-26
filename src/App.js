2d25902286e3241dea9a084364fd6ae64a0bc48b
kdw <zkdlwu94@naver.com>
2021-07-26 22:24:44 +0900

    fi

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Header from "./component/Header/Header";
 import { BrowserRouter, Route, Link, Switch } from "react-router-dom";
 import Home from "./component/Home/Home";
 import Admin from "./component/Admin/Admin";
-import { useState } from "react";
+import React,{ useState } from "react";
 function App() {
 
   const [menuOpen, setMenuOpen] = useState(false);
--- src/component/Header/Header.js
+++ src/component/Header/Header.js
@@ -1,8 +1,8 @@
 import FacebookIcon from '@material-ui/icons/Facebook';
 import GitHubIcon from '@material-ui/icons/GitHub';
 import LinkedInIcon from '@material-ui/icons/LinkedIn';
-
-
+import React from 'react';
+import { common } from '@material-ui/core/colors';
 import './Header.css'
 
 const Header = ({menuOpen,setMenuOpen}) =>{
@@ -16,25 +16,25 @@ const Header = ({menuOpen,setMenuOpen}) =>{
         <div className={"topbar " + (menuOpen && "active")}>
             <div className="wrapper">
                 <div className="left">
-                    <a href="#intro" className="logo">My Story.</a>
+                <a href="https://github.com/Dongwankim1">
+                        <FacebookIcon   style={menuOpen ? ({ color: common.white }) : null} fontSize="large"/>
+                        </a>
+                        <a href="https://github.com/Dongwankim1">
+                        <GitHubIcon style={menuOpen ? ({ color: common.white }) : null} fontSize="large"/>
+                        </a>
+                        <a href="https://www.linkedin.com/in/%EB%94%94%EC%B0%8C-hip-05a2791a2/">
+                        <LinkedInIcon style={menuOpen ? ({ color: common.white }) : null} fontSize="large"/>
+                    </a>
                     <div className="itemContainer">
                  
 
                     </div>
                 </div>
                 <div className="middle">
-                <a href="https://github.com/Dongwankim1">
-                        <FacebookIcon fontSize="large"/>
-                        </a>
-                        <a href="https://github.com/Dongwankim1">
-                        <GitHubIcon fontSize="large"/>
-                        </a>
-                        <a href="https://www.linkedin.com/in/%EB%94%94%EC%B0%8C-hip-05a2791a2/">
-                        <LinkedInIcon fontSize="large"/>
-                    </a>
+              
                 </div>
                 <div className="right">
-                    <div className="hamburger" onClick={handleMenu} >
+                    <div className={"hamburger " + (menuOpen && "active")} onClick={handleMenu} >
                         <span className="line1"></span>
                         <span className="line2"></span>
                         <span className="line3"></span>
