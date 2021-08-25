8b7e82ebffedde7174a986f002098451ef5c9ee2
kdw <zkdlwu94@naver.com>
2021-08-25 21:11:23 +0900

    update scroll snpa

--- src/component/Home/Home.js
+++ src/component/Home/Home.js
@@ -1,14 +1,15 @@
 import React from 'react'
 import Intro from './Intro/Intro'
 import './Home.css';
+import Portfolio from './Protfolio/Portfolio';
 export default function Home() {
     return (
    
             
              <div className="sections">
                  <Intro />
-                {/*<Portfolio />
-                <Works />
+                <Portfolio />
+                {/*<Works />
                 <Testimonials />
                 <Contact /> */}
             </div>
